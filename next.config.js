ad6cc40b9913228ab5864196cfc1602fa04be90b
ragnorcap <alexfant803@gmail.com>
2025-04-05 20:07:17 -0400

    Fix config

--- next.config.js
+++ next.config.js
@@ -3,9 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   trailingSlash: false,
   assetPrefix: process.env.NODE_ENV === 'production' ? undefined : undefined,
-  experimental: {
-    strictMode: true,
-  },
 }
 
 module.exports = nextConfig 
